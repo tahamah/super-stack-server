93e3c436b8a3a467eaa0114c76006d90842a2d86
unknown <taha.iu.bd@gmail.com>
2022-04-29 11:04:17 +0600

    env-add

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri =
-    'mongodb+srv://warehouse-manager:kSKGs4onMoQQfLit@cluster0.jvufd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jvufd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
